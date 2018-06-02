f6480dc2aa23b0834e38552ac5ee23c2f82c30d4
Tooraj Khatibi <2rajpx@gmail.com>
2018-06-02 15:13:58 +0430

    update(typography): code styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,6 +5,36 @@ Wordpress2016.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
+  'img': {
+    width: '100%',
+  },
+
+  'code.language-text': {
+    'background-color': '#EEE',
+    color: 'red',
+    'padding-left': '5px',
+    'padding-right': '5px',
+    direction: 'ltr',
+  },
+  'a > code.language-text': {
+    color: 'purple',
+  },
+
+  'pre': {
+    padding: '5px',
+    overflow: 'auto',
+    direction: 'ltr',
+  },
+  'pre.language-command': {
+    'background-color': 'black',
+    color: 'white',
+  },
+  'pre.language-markdown': {
+    'background-color': '#EEE',
+  },
+  'pre.language-javascript': {
+    'background-color': '#EEE',
+  },
 })
 
 delete Wordpress2016.googleFonts
